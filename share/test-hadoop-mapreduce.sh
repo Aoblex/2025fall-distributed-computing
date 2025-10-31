a1fb0c1bbb36b19fabc29273311e1886c8fcce1f
Zhiguo Mi <trytrywang@126.com>
2025-10-31 21:19:30 +0800

    add test for hadoop mapreduce

---
+++ share/test-hadoop-mapreduce.sh
@@ -0,0 +1,19 @@
+touch /root/share/input.txt
+cat <<EOF > /root/share/input.txt
+Hello Hadoop
+Hello World
+Hadoop is a framework
+Hadoop MapReduce is powerful
+MapReduce simplifies big data processing
+EOF
+
+hdfs dfs -mkdir -p /test/input
+hdfs dfs -put /root/share/input.txt /test/input
+hdfs dfs -rm -r /test/output || true
+
+hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar \
+    wordcount \
+    /test/input \
+    /test/output
+
+hdfs dfs -cat /test/output/part-r-00000

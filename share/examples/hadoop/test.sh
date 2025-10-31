ecf689b270b5b6b2e1b5143a89a356e036e16d8f
Zhiguo Mi <trytrywang@126.com>
2025-10-31 22:19:08 +0800

    update examples

--- share/examples/hadoop/test.sh
+++ share/examples/hadoop/test.sh
@@ -1,5 +1,8 @@
-touch /root/share/input.txt
-cat <<EOF > /root/share/input.txt
+HADOOP_EXAMPLE="/root/share/examples/hadoop"
+mkdir -p "$HADOOP_EXAMPLE"
+
+touch "$HADOOP_EXAMPLE/input.txt"
+cat <<EOF > "$HADOOP_EXAMPLE/input.txt"
 Hello Hadoop
 Hello World
 Hadoop is a framework
@@ -8,7 +11,7 @@ MapReduce simplifies big data processing
 EOF
 
 hdfs dfs -mkdir -p /test/input
-hdfs dfs -put -f /root/share/input.txt /test/input
+hdfs dfs -put -f "$HADOOP_EXAMPLE/input.txt" /test/input
 hdfs dfs -rm -r -f /test/output
 
 hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar \
